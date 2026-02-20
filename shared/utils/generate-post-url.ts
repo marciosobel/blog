15c9b9a9798a2e318d3f4eda49a178d11fda3815
marciosobel <marciosobel@proton.me>
2026-02-20 05:03:35 -0300

    move utils to shared

--- shared/utils/generate-post-url.ts
+++ shared/utils/generate-post-url.ts
@@ -1,10 +1,11 @@
+/** RegEx to extract the post name from a URL by taking the part between the first and last slashes */
 const regex = /[^\/]+\/(.+)\/[^\/]+/;
 
 export default function (post: string, locale: string): string {
   const name = post.match(regex)?.[1];
   if (locale == "en") {
-    return `/${name}`;
+    return `${name}`;
   }
 
-  return `/${locale}/${name}`;
+  return `${locale}/${name}`;
 }
