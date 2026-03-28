7334635e356413f41df63fda8d784168c81b8631
Márcio Sobel <marciosobel@proton.me>
2026-03-28 11:03:33 -0300

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "PNPM development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+  }: let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    devShells = eachSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodejs
+            pnpm
+            vtsls
+          ];
+        };
+      }
+    );
+  };
+}
